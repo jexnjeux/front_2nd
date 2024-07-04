babcfb0ac5b63c9d41f8912f19170a4aa5f9c551
jexnjeux <zieun.na@gmail.com>
2024-07-05 04:40:26 +0900

    advanced

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,15 +1,57 @@
 export function createHooks(callback) {
+  let index = 0;
+  let state = [];
+  let setters = [];
+  let memoizedValues = [];
+  let memoizedDependencies = [];
+  let renderQueue = null;
+
+  const setState = (index) => (newState) => {
+    if (state[index] !== newState) {
+      state[index] = newState;
+      if (renderQueue !== null) {
+        clearTimeout(renderQueue);
+      }
+      renderQueue = setTimeout(() => {
+        resetContext();
+        callback();
+        renderQueue = null;
+      }, 0);
+    }
+  };
+
   const useState = (initState) => {
-    return [];
+    if (state.length === index) {
+      state.push(initState);
+      setters.push(setState(index));
+    }
+    const value = state[index];
+    const setter = setters[index];
+    index++;
+    return [value, setter];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    if (memoizedValues.length === index) {
+      memoizedValues.push(fn());
+      memoizedDependencies.push(refs);
+    } else {
+      const isDependenciesChanged = refs.some(
+        (ref, i) => ref !== memoizedDependencies[index][i]
+      );
+      if (isDependenciesChanged) {
+        memoizedValues[index] = fn();
+        memoizedDependencies[index] = refs;
+      }
+    }
+    const value = memoizedValues[index];
+    index++;
+    return value;
   };
 
   const resetContext = () => {
-
-  }
+    index = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
