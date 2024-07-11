682f8b5b3222cb8cbe27e7ef9cfe17a29ba3ca15
jexnjeux <zieun.na@gmail.com>
2024-07-12 03:57:47 +0900

    eslint, prettier

--- packages/assignment-4/.eslintrc.cjs
+++ packages/assignment-4/.eslintrc.cjs
@@ -1,11 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  ignorePatterns: ['.eslintrc.cjs'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
   rules: {},
-}
+};
