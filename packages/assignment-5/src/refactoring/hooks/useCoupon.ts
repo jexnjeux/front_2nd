2ca966741ea3551727a271ab2ee5e9ca238e31c6
jexnjeux <zieun.na@gmail.com>
2024-07-18 01:54:51 +0900

    basic

--- packages/assignment-5/src/refactoring/hooks/useCoupon.ts
+++ packages/assignment-5/src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,18 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { Coupon } from '../../types.ts';
+import { useState } from 'react';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons || []);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+
+  const deleteCoupon = (deletedCoupon: Coupon) => {
+    setCoupons((prevCoupons) =>
+      prevCoupons.filter((coupon) => coupon.code !== deletedCoupon.code)
+    );
+  };
+
+  return { coupons, addCoupon, deleteCoupon };
 };
--- packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
+++ packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,110 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon, Discount } from '../../../types';
+
+const getSortedDiscounts = (discounts: Discount[]) => {
+  return discounts.sort((a, b) => b.quantity - a.quantity);
+};
+
+const getDiscountRate = (discounts: Discount[], quantity: number) => {
+  let discountRate = 0;
+  for (const discount of discounts) {
+    if (quantity >= discount.quantity) {
+      discountRate = discount.rate;
+      break;
+    }
+  }
+  return discountRate;
+};
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const { product, quantity } = item;
+  const sortedDiscounts = getSortedDiscounts(product.discounts);
+  const discountRate = getDiscountRate(sortedDiscounts, quantity);
+
+  return product.price * quantity * (1 - discountRate);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const {
+    product: { discounts },
+    quantity,
+  } = item;
+  let discountRate = 0;
+  const sortedDiscounts = getSortedDiscounts(discounts);
+  for (const discount of sortedDiscounts) {
+    if (quantity >= discount.quantity) {
+      discountRate = discount.rate;
+      break;
+    }
+  }
+  return discountRate;
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+const calculateTotalBeforeDiscount = (cart: CartItem[]) => {
+  return cart.reduce(
+    (acc, item) => acc + item.product.price * item.quantity,
+    0
+  );
+};
+
+const calculateTotalAfterDiscount = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
+  let totalAfterDiscount = cart.reduce(
+    (acc, item) => acc + calculateItemTotal(item),
+    0
+  );
+
+  if (selectedCoupon) {
+    totalAfterDiscount = applyCoupon(totalAfterDiscount, selectedCoupon);
+  }
+  return totalAfterDiscount;
+};
+
+const applyCoupon = (total: number, coupon: Coupon) => {
+  if (coupon?.discountType === 'amount') {
+    total = total - coupon.discountValue;
+  } else if (coupon?.discountType === 'percentage') {
+    const discountRate = coupon.discountValue / 100;
+    total = total * (1 - discountRate);
+  }
+  return total;
+};
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
+  const totalBeforeDiscount = calculateTotalBeforeDiscount(cart);
+  const totalAfterDiscount = calculateTotalAfterDiscount(cart, selectedCoupon);
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number
+): CartItem[] => {
+  if (newQuantity <= 0) {
+    return cart.filter((item) => item.product.id !== productId);
+  }
+  const newCart = cart.map((item) => {
+    const {
+      product: { id, stock },
+    } = item;
+    if (id === productId) {
+      const updatedQuantity = stock >= newQuantity ? newQuantity : stock;
+      return {
+        ...item,
+        quantity: updatedQuantity,
+      };
+    }
+    return item;
+  });
+  return newCart;
 };
